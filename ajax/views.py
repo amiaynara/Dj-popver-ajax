2faca985027abbecc29e0cceef39844cb2111149
Amiay Narayan <amiayn@iitbhilai.ac.in>
2021-05-17 20:06:53 +0530

    Start project

---
+++ ajax/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+# Create your views here.
+
+def PopoverView(request):
+    return render(request, 'ajax/home.html')
+
+def DataView(request):
+    context={
+            "name": "amiay",
+            "last_name": "narayan",
+            }
+    return JsonResponse(context)
